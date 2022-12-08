8607e9a83efbecd7bcbdc3ec4257c137783c35c0
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2022-12-08 19:43:41 +0300

    user limit

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,10 @@
+# User limit
+file {'/etc/security/limits.conf':
+  ensure => present,
+}
 
+exec { 'change user limits':
+  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 4000/g" /etc/security/limits.conf;\
+  sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/g" /etc/security/limits.conf',
+  provider => 'shell',
+}
