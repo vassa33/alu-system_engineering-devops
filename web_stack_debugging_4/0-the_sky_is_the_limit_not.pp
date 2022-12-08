bcf2b854b61ede004a4c084952c3f725be373c17
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2022-12-08 19:40:27 +0300

    sky limit?

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Nginx service; increase ULIMIT
-service { 'nginx':
+service { '/nginx':
   ensure => running,
 }
 
-exec { 'increase ULIMIT':
+exec { 'increase the ULIMIT':
   command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4000\"/g" /etc/default/nginx',
   provider => 'shell',
   notify   => Service['nginx']
