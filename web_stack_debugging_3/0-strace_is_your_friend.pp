b4c26dc2da1060ec3d655f46780c36f10249d0ec
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2022-12-08 13:28:20 +0300

    strace your way out

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,5 @@
-
+# using puppet: correct class-wp-locale.phpp to class-wp-locale.php in wp-settings.php
+exec { 'fix naming error':
+  command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+  provider => 'shell'
+}
