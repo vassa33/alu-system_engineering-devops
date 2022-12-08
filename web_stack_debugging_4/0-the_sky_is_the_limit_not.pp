5c86b8baa18be971ac80239e2f517607ac577faf
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2022-12-08 19:35:04 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,10 @@
+# Nginx service; increase ULIMIT
+service { 'nginx':
+  ensure => running,
+}
 
+exec { 'increase ULIMIT':
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4000\"/g" /etc/default/nginx',
+  provider => 'shell',
+  notify   => Service['nginx']
+}
